3cb650dbdd4ea7de6f1aaa28775726541b5d1761
Eric Gorr <ericgorr@mac.com>
2013-05-25 20:31:45 -0400

    Added some code to demonstrate a scrolling problem. In the sc_technique app, if all of the content in the middle column cannot be displayed, select an item and then use the arrow keys to move down the list. Items that appear off the bottom are not drawn correctly. Using the scrollbar works as expected.

--- apps/sc_technique/views/folderDisplayView.js
+++ apps/sc_technique/views/folderDisplayView.js
@@ -1,24 +1,28 @@
 ScTechnique.FolderDisplayView = SC.View.extend({
 
-	childViews:	[ 'folders', 'file' ],
+	childViews:	[ 'foldersscroller', 'file' ],
 	
-	folders:	SC.ListView.design({
-		layout:				{ width: 300 },
-		rowHeight:			18,
-		selectionBinding:	'ScTechnique.folderArrayController.selection',
-		contentBinding:		'ScTechnique.folderArrayController.arrangedObjects',
-		action:				'actionFileSelected',
-		actOnSelect:		true,
-		
-		exampleView: 	SC.LabelView.design({
-		
-							valueBinding:	SC.Binding.oneWay( '.content' ).transform( function( content ) {	
+	foldersscroller:    SC.ScrollView.extend({
 
-								return content.thename;
-
-							})
-						})
-	}),
+		                    layout:			{ width: 300 },
+                            contentView:    SC.ListView.design({
+                                                rowHeight:			18,
+                                                selectionBinding:	'ScTechnique.folderArrayController.selection',
+                                                contentBinding:		'ScTechnique.folderArrayController.arrangedObjects',
+                                                action:				'actionFileSelected',
+                                                actOnSelect:		true,
+                                                
+                                                exampleView: 	SC.LabelView.design({
+                                                
+                                                                    valueBinding:	SC.Binding.oneWay( '.content' ).transform( function( content ) {	
+                                        
+                                                                        return content.thename;
+                                        
+                                                                    })
+                                                                })
+                                            }),
+                        }),
+	                    
 	
 	file:	SC.TextFieldView.design({
 		isTextArea: true,
